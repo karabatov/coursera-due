40b5e96745826479d60e6dec9b4d8f6caef362c2
Yuri Karabatov <karabatov@gmail.com>
2014-01-31 17:38:54 +0400

    Second subclass for CDViewController.

---
+++ Coursera Due/Coursera Due/CDCourseViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDCourseViewController.h
+//  Coursera Due
+//
+//  Created by Yuri Karabatov on 31.01.14.
+//  Copyright (c) 2014 Yuri Karabatov. All rights reserved.
+//
+
+#import "CDViewController.h"
+
+@interface CDCourseViewController : CDViewController
+
+@end
